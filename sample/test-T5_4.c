cc8b6f4afd98f9cca4246fb6638c97e577f5b306
pokutuna <popopopopokutuna@gmail.com>
2011-10-27 18:18:59 +0900

    fix readme & sample code

--- sample/test-T5_4.c
+++ sample/test-T5_4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "minunit.h"
 
-int cubic(int); // (1) テストしたい関数のプロトタイプ
-
 int mu_nfail = 0; int mu_ntest = 0; int mu_nassert = 0;
 
-static char* test_cubic()
+int cubic(int); // (1) テストしたい関数のプロトタイプ
+
+static char* test_cubic() // (2) 1つ目のテスト
 {
-    mu_assert(cubic(2) == 8);
+    mu_assert(cubic(2) == 8); // (2) アサーション
     mu_assert(cubic(5) == 125);
     mu_assert(cubic(100) == 1000000);
     return 0;
 }
 
-static char* test_cubic_edge_case()
+static char* test_cubic_edge_case() // (2) 2つ目のテスト
 {
     mu_assert(cubic(0) == 0);
     mu_assert(cubic(-1) == -1);
@@ -21,6 +21,7 @@ static char* test_cubic_edge_case()
 
 static char* all_tests()
 {
+    // (3) テスト関数を呼ぶ
     mu_run_test(test_cubic);
     mu_run_test(test_cubic_edge_case);
     return 0;
